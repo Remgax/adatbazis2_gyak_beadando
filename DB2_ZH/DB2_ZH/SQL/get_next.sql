1594887ad263391b5d641c42bacc9abb17be48ac
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 00:33:44 +0100

    get_next Fuction

--- DB2_ZH/DB2_ZH/SQL/get_next.sql
+++ DB2_ZH/DB2_ZH/SQL/get_next.sql
@@ -1,11 +1,16 @@
-﻿CREATE OR REPLACE FUNCTION get_next(
-	p_id in szamlak.id%TYPE
-)
-RETURN INT
-DETERMINISTIC
-AUTHID CURRENCT_USER
-AS
-	v_Id INT;
+﻿create or replace FUNCTION get_next
+RETURN szamlak.id%TYPE 
+IS
+nextID szamlak.id%TYPE;
+v_db number;
+
 BEGIN
-	IF p_id > 
-END get_next;
+    SELECT COUNT(id) into v_db FROM szamlak;
+    IF v_db <> 0 THEN
+        SELECT MIN(id)-3 INTO nextID FROM szamlak;
+        
+    ELSE
+        nextID := 10000;
+    END IF;
+    RETURN nextID;
+END;
