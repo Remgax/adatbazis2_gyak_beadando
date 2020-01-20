8b43026c10d4b623ccec3fdf210fa6291feea950
Milán Kormány <kormany.1993@gmail.com>
2020-01-20 13:43:40 +0100

    Fájl név javitás, 5. feladat első rész
    Fájlnév egységesítése a leírás alapján.
    Record.cs osztály elkészítése.

--- DB2_ZH/DB2_ZH/Models/Records/Record.cs
+++ DB2_ZH/DB2_ZH/Models/Records/Record.cs
@@ -8,5 +8,34 @@ namespace DB2_ZH.Models.Records
 {
     class Record
     {
+       private int id;
+       public int Id
+       {
+           get { return id; }
+           set { id = value; }
+       }
+       
+       private string nev;
+       public string Nev
+       {
+           get { return nev; }
+           set { nev = value; }
+       }
+
+       private string bankartyaszam;
+       public string Bankartyaszam
+       {
+           get { return bankartyaszam; }
+           set { bankartyaszam = value; }
+       }
+       
+       private int egyenleg;
+       public int Egyenleg
+       {
+           get { return egyenleg; }
+           set { egyenleg = value; }
+       }
+       
+
     }
 }
