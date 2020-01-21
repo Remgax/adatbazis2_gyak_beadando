74fd8ab1b1f08a8c64d037f6c154dd92dfff47b9
Milán Kormány <kormany.1993@gmail.com>
2020-01-21 02:09:50 +0100

    buttoInsert create

--- DB2_ZH/DB2_ZH/Views/WinForms/ZHForm.cs
+++ DB2_ZH/DB2_ZH/Views/WinForms/ZHForm.cs
@@ -14,10 +14,13 @@ namespace DB2_ZH.Views.WinForms
     public partial class ZHForm : Form
     {
         private OracleManager oracleManager;
-
+        private OracleManager tableManager;
+        private List<Models.Records.Record> szamlak;
         public ZHForm()
         {
             InitializeComponent();
+            tableManager = new OracleManager();
+            szamlak = tableManager.Select();
         }
 
         private void ZHForm_Load(object sender, EventArgs e)
@@ -32,7 +35,19 @@ namespace DB2_ZH.Views.WinForms
 
         private void button_insert_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Models.Records.Record ujRecord = new Models.Records.Record();
+                ujRecord.Nev = textBox_nev.Text;
+                ujRecord.Bankartyaszam = textBox_kartyaszam.Text;
+                ujRecord.Egyenleg = int.Parse(textBox_egyenleg.Text);
+                int ujSorok = tableManager.Insert(ujRecord);
+                MessageBox.Show(ujSorok + " sor rögzitésre került!");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
 
         private void button_delete_Click(object sender, EventArgs e)
